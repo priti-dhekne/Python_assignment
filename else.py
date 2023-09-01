2c22c3d5a7feb9fc61ad7527c84b257e8cfd2569
Priti <142306089+priti-dhekne@users.noreply.github.com>
2023-09-01 20:33:16 +0530

    Add files via upload

---
+++ else.py
@@ -0,0 +1,8 @@
+a=int(input("a="))
+b=int(input("b="))
+if a==b:
+    print("True")
+else:
+    print("False")    
+
+
---
+++ power.py
@@ -0,0 +1,3 @@
+a=int(input("a="))
+b=int(input("b="))
+print("{a}**{b}={c}".format(a=a,b=b,c=a**b))
